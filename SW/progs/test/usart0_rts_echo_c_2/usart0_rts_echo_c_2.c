0cd36e77ba74fb7624304d8fc69b85f9e18b8fba
gilhad <gilhad@seznam.cz>
2025-12-09 02:40:21 +0100

    fixed usart0_rts_echo_c_2 RTS/CTS

--- SW/progs/test/usart0_rts_echo_c_2/usart0_rts_echo_c_2.c
+++ SW/progs/test/usart0_rts_echo_c_2/usart0_rts_echo_c_2.c
@@ -2,13 +2,19 @@
 #include <avr/interrupt.h>
 #include "Serial.RTS/usart0/usart0.h"
 // LIBS: Serial.RTS/usart0
-
-
+/*
+char dta[100]="1234567890asdfghjkl";
+int dc=20;
+*/
+uint16_t zz;
 void setup(void) {
 	usart0_setup();
 	DDRF = 0xFF;
 	DDRK = 0xFF;
 	sei();
+	TX0_Write('#');
+	TX0_Write('#');
+	TX0_Write('#');
 }
 
 void loop(void) {
@@ -17,13 +23,29 @@ void loop(void) {
 
 	uint8_t tx_used = TX0_Used();
 	PORTF = tx_used;
-
+	/*
+	if (rx_avail) dta[dc++] = RX0_Read() & 0xFF;
+	if (dc>10) {
+		for (int i=0;i<dc;i++) TX0_Write(dta[i]);
+		dc=0;
+		};
+	*/
 	uint16_t ch = RX0_Read();
 	if (ch >> 8) { // Pokud r25 != 0
 		TX0_Write((char)(ch & 0xFF)); // Blokuje dokud není volno
 		if ((ch & 0xFF)==10)
 			TX0_Write(13); // Blokuje dokud není volno
-	}
+		if ((ch & 0xFF)==13)
+			TX0_Write(10); // Blokuje dokud není volno
+		
+//		TX0_Write(' ');
+//		TX0_Write((ch & 0xFF));
+//		TX0_Write('<');
+//		TX0_Write('1');
+//		TX0_Write('>');
+	};
+//	for (int i=0;i<8*256;i++) zz+=PORTF;
+
 }
 
 int main(void) {
