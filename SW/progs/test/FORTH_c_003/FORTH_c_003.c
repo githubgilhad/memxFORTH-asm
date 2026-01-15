2ab8bcd1ed4be13ad209f58adf95025372981a3d
gilhad <gilhad@seznam.cz>
2026-01-15 10:04:11 +0100

    +FORTH_c_003.c - may start testing

---
+++ SW/progs/test/FORTH_c_003/FORTH_c_003.c
@@ -0,0 +1,33 @@
+#include <avr/io.h>
+#include <avr/interrupt.h>
+// LIBS: Serial.RTS/usart0 FORTH/FORTH-Engine FORTH/FORTH-Memory
+
+#include "Serial.RTS/usart0/usart0.h"
+#include "FORTH/FORTH-Engine/FORTH-Engine.h"
+
+#define TEXT __attribute__((section(".text")))
+
+TEXT void setup(void) {
+	usart0_setup();
+	sei();
+}
+TEXT void loop(void) {
+
+	uint16_t ch = RX0_Read();
+	if (ch >> 8) { // Pokud r25 != 0
+		TX0_Write((char)(ch & 0xFF)); // Blokuje dokud není volno
+		if ((ch & 0xFF)==13)
+			TX0_Write(10); // Blokuje dokud není volno
+	};
+//	for (int i=0;i<256;i++) ch=PORTF;
+//	__asm__ __volatile__ ("jmp NEXT \n\t");
+//	NEXT();
+}
+
+TEXT int main(void) {
+	setup();
+	while (1) {
+		loop();
+	}
+}
+
---
+++ SW/progs/test/FORTH_c_003/README.rst
@@ -0,0 +1,3 @@
+Simple RTS/CTS USART echo, without LEDs on PortF and PortK showing usage of RX and TX buffers
+use speed 115200
+Rewritten to C, with prologue and INTs via __vector_xx
